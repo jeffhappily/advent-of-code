2187e7b88d236fa9367258f4904e4725e74d9cd7
Jeff Cheah <goodboy96@live.com.my>
2020-12-08 18:51:38 +0800

    Finish p2

--- d4.hs
+++ d4.hs
@@ -4,13 +4,22 @@
     --ghc-options -Wall
     --package attoparsec
     --package text
+    --package either
 -}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards #-}
-import Data.Attoparsec.Text
-import qualified Data.Text.IO as T (readFile)
+{-# LANGUAGE RecordWildCards   #-}
+import           Control.Applicative     ((<|>))
+import           Data.Attoparsec.Text
+import           Data.Either             (isRight)
+import           Data.Either.Combinators (rightToMaybe)
+import           Data.Functor            (($>))
+import           Data.Maybe              (isJust)
+import qualified Data.Text               as T
+import qualified Data.Text.IO            as T (readFile)
+import           Prelude                 hiding (take)
+import           Text.Read               (readMaybe)
 
-data Field
+data FieldType
   = BYR -- ^ Birth Year
   | IYR -- ^ Issue Year
   | EYR -- ^ Expiration Year
@@ -19,88 +28,146 @@ data Field
   | ECL -- ^ Eye Color
   | PID -- ^ Passport ID
   | CID -- ^ Country ID
-  deriving Show
+  deriving (Eq, Show)
 
-alphaNumHash :: Parser Char
-alphaNumHash = satisfy $ inClass "a-z0-9#"
+data Field = Field FieldType T.Text
+  deriving (Eq, Show)
 
-byrParser :: Parser Field
-byrParser = "byr:" *> many1 alphaNumHash *> pure BYR
+alphaNumHash :: Char -> Bool
+alphaNumHash = inClass "a-z0-9#"
 
-iyrParser :: Parser Field
-iyrParser = "iyr:" *> many1 alphaNumHash *> pure IYR
+byrParser :: Parser FieldType
+byrParser = "byr:" $> BYR
 
-eyrParser :: Parser Field
-eyrParser = "eyr:" *> many1 alphaNumHash *> pure EYR
+iyrParser :: Parser FieldType
+iyrParser = "iyr:" $> IYR
 
-hgtParser :: Parser Field
-hgtParser = "hgt:" *> many1 alphaNumHash *> pure HGT
+eyrParser :: Parser FieldType
+eyrParser = "eyr:" $> EYR
 
-hclParser :: Parser Field
-hclParser = "hcl:" *> many1 alphaNumHash *> pure HCL
+hgtParser :: Parser FieldType
+hgtParser = "hgt:" $> HGT
 
-eclParser :: Parser Field
-eclParser = "ecl:" *> many1 alphaNumHash *> pure ECL
+hclParser :: Parser FieldType
+hclParser = "hcl:" $> HCL
 
-pidParser :: Parser Field
-pidParser = "pid:" *> many1 alphaNumHash *> pure PID
+eclParser :: Parser FieldType
+eclParser = "ecl:" $> ECL
 
-cidParser :: Parser Field
-cidParser = "cid:" *> many1 alphaNumHash *> pure CID
+pidParser :: Parser FieldType
+pidParser = "pid:" $> PID
+
+cidParser :: Parser FieldType
+cidParser = "cid:" $> CID
 
 fieldParser :: Parser Field
-fieldParser = choice
-  [ byrParser
-  , iyrParser
-  , eyrParser
-  , hgtParser
-  , hclParser
-  , eclParser
-  , pidParser
-  , cidParser
-  ]
+fieldParser = Field <$> fieldType <*> takeWhile1 alphaNumHash
+  where
+    fieldType = choice
+      [ byrParser
+      , iyrParser
+      , eyrParser
+      , hgtParser
+      , hclParser
+      , eclParser
+      , pidParser
+      , cidParser
+      ]
 
--- | 8 bit value to indicate the presence of equivalent field
+-- | Passport and its fields
 data Passport =
   Passport
-    { byr :: Bool
-    , iyr :: Bool
-    , eyr :: Bool
-    , hgt :: Bool
-    , hcl :: Bool
-    , ecl :: Bool
-    , pid :: Bool
-    , cid :: Bool
+    { byr :: Maybe Field
+    , iyr :: Maybe Field
+    , eyr :: Maybe Field
+    , hgt :: Maybe Field
+    , hcl :: Maybe Field
+    , ecl :: Maybe Field
+    , pid :: Maybe Field
+    , cid :: Maybe Field
     }
   deriving (Eq, Show)
 
+
 emptyPassport :: Passport
-emptyPassport = Passport False False False False False False False False
+emptyPassport = Passport Nothing Nothing Nothing Nothing Nothing Nothing Nothing Nothing
 
 passportParser :: Parser Passport
 passportParser = do
-  fields <- many1 (fieldParser <* space)
+  fields <- many1 (fieldParser <* try space)
 
   pure $
-    foldr (\x z@(Passport {..}) -> 
-      -- Toggle field
+    foldr (\x z@Passport {..} ->
+      -- Toggle field, to avoid having more than one of the same field
       case x of
-        BYR -> z { byr = not byr }
-        IYR -> z { iyr = not iyr }
-        EYR -> z { eyr = not eyr }
-        HGT -> z { hgt = not hgt }
-        HCL -> z { hcl = not hcl }
-        ECL -> z { ecl = not ecl }
-        PID -> z { pid = not pid }
-        CID -> z { cid = not cid }
+        f@(Field BYR _) -> z { byr = maybe (Just f) (const Nothing) byr }
+        f@(Field IYR _) -> z { iyr = maybe (Just f) (const Nothing) iyr }
+        f@(Field EYR _) -> z { eyr = maybe (Just f) (const Nothing) eyr }
+        f@(Field HGT _) -> z { hgt = maybe (Just f) (const Nothing) hgt }
+        f@(Field HCL _) -> z { hcl = maybe (Just f) (const Nothing) hcl }
+        f@(Field ECL _) -> z { ecl = maybe (Just f) (const Nothing) ecl }
+        f@(Field PID _) -> z { pid = maybe (Just f) (const Nothing) pid }
+        f@(Field CID _) -> z { cid = maybe (Just f) (const Nothing) cid }
     ) emptyPassport fields
 
 isPassportValid :: Passport -> Bool
-isPassportValid (Passport True True True True True True True _) = True
+isPassportValid (Passport (Just _) (Just _) (Just _) (Just _) (Just _) (Just _) (Just _) _) = True
 isPassportValid _ = False
 
+data HeightUnit = Cm | In
+  deriving Show
+data Height = Height Int HeightUnit
+  deriving Show
+
+heightUnitParser :: Parser HeightUnit
+heightUnitParser =
+      string "cm" $> Cm
+  <|> string "in" $> In
+
+heightParser :: Parser Height
+heightParser =
+  Height <$> decimal <*> heightUnitParser <* endOfInput
+
+hairColorParser :: Parser ()
+hairColorParser = char '#' *> count 6 (satisfy alphaNumHash) *> endOfInput
+
+isPassportValid2 :: Passport -> Bool
+isPassportValid2 (Passport
+  (Just (Field _ byr))
+  (Just (Field _ iyr))
+  (Just (Field _ eyr))
+  (Just (Field _ hgt))
+  (Just (Field _ hcl))
+  (Just (Field _ ecl))
+  (Just (Field _ pid))
+  _) = and
+    [ validByr
+    , validIyr
+    , validEyr
+    , validHgt
+    , validHcl
+    , validEcl
+    , validPid
+    ]
+    where
+      validByr = validateYear byr 1920 2002
+      validIyr = validateYear iyr 2010 2020
+      validEyr = validateYear eyr 2020 2030
+      validHgt = maybe False validateHeight $ rightToMaybe (parseOnly heightParser hgt)
+      validHcl = isRight $ parseOnly hairColorParser hcl
+      validEcl = ecl `elem` ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
+      validPid = T.length pid == 9 && isJust (readMaybe (T.unpack pid) :: Maybe Int)
+
+      validateYear :: T.Text -> Int -> Int -> Bool
+      validateYear year l u = T.length year == 4 && maybe False (\x -> x >= l && x <= u) (readMaybe (T.unpack year))
+
+      validateHeight (Height h Cm) = h >= 150 && h <= 193
+      validateHeight (Height h In) = h >= 59 && h <= 76
+
+isPassportValid2 _ = False
+
 inputParser :: Parser [Passport]
-inputParser = many1 (passportParser <* many' space)
+inputParser = many1 (passportParser <* skipSpace)
 
 main :: IO ()
 main = do
@@ -108,6 +175,11 @@ main = do
   case parseOnly inputParser content of
     Left err -> error err
     Right p -> do
+      putStrLn "Part 1"
       print $
-       length $ filter isPassportValid p
+        length $ filter isPassportValid p
+
+      putStrLn "Part 2"
+      print $
+        length $ filter isPassportValid2 p
 
