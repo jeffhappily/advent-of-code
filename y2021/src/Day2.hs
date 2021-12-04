272ff29926323c257cb00061d5830185722e2dc6
Jeff Happily <goodboy96@live.com.my>
2021-12-04 08:57:19 +0800

    Day 2 part 2

--- y2021/src/Day2.hs
+++ y2021/src/Day2.hs
@@ -4,6 +4,7 @@ module Day2 (main) where
 
 import Control.Applicative (Alternative ((<|>)))
 import Data.Attoparsec.Text
+import Data.List (foldl')
 import Data.Monoid (Sum (Sum))
 import Data.Text.IO qualified as T (readFile)
 import GHC.Generics (Generic)
@@ -13,6 +14,7 @@ import Prelude
 data Position = Position
   { hozirontal :: Sum Int
   , depth :: Sum Int
+  , aim :: Sum Int
   }
   deriving stock (Generic)
   deriving (Semigroup, Monoid) via (Generically Position)
@@ -22,14 +24,15 @@ data Command
   | Down Int
   | Up Int
 
+-- | Part 1
 commandToPosition :: Command -> Position
 commandToPosition = \case
-  Forward (Sum -> n) -> Position n mempty
-  Down (Sum -> n) -> Position mempty n
-  Up (Sum -> n) -> Position mempty (negate <$> n)
+  Forward (Sum -> n) -> Position n mempty mempty
+  Down (Sum -> n) -> Position mempty n mempty
+  Up (Sum -> n) -> Position mempty (negate <$> n) mempty
 
 positionToAnswer :: Position -> Int
-positionToAnswer (Position (Sum x) (Sum y)) = x * y
+positionToAnswer (Position (Sum x) (Sum y) _) = x * y
 
 parseForward :: Parser Command
 parseForward = Forward <$> (asciiCI "forward " >> decimal)
@@ -46,6 +49,17 @@ parseCommand = parseForward <|> parseDown <|> parseUp
 parseInput :: Parser [Command]
 parseInput = many' $ parseCommand <* endOfLine
 
+{- | Part 2
+ commandToPosition' :: Command -> Position
+-}
+mergeCommand :: Position -> Command -> Position
+mergeCommand pos@(Position _ _ aim') c =
+  let newPos = case c of
+        Forward (Sum -> n) -> Position n (aim' * n) mempty
+        Down (Sum -> n) -> Position mempty mempty n
+        Up (Sum -> n) -> Position mempty mempty (negate <$> n)
+   in pos <> newPos
+
 main :: IO ()
 main = do
   content <- T.readFile "y2021/input/d2.txt"
@@ -56,3 +70,8 @@ main = do
       let ans = positionToAnswer res
       putStr "Part 1: "
       print ans
+
+      let res2 = foldl' mergeCommand mempty input
+      let ans2 = positionToAnswer res2
+      putStr "Part 2: "
+      print ans2
